475d2e345664a7d30a0b8b8c950710817fe81dbd
whiteBeardC <1099177741@qq.com>
2020-07-05 22:33:15 +0800

    translate tabs to spaces

--- high_precision_addition.c
+++ high_precision_addition.c
@@ -6,35 +6,35 @@
 
 string Addition(const string &lhs, const string &rhs)
 {
-	string str1 = lhs;
-	string str2 = rhs;
+    string str1 = lhs;
+    string str2 = rhs;
 
-	// make sure the size of str1 is larger than that of str2.
-	if (str1.size() > str2.size())
-	{
-		swap(str1, str2);
-	}
+    // make sure the size of str1 is larger than that of str2.
+    if (str1.size() > str2.size())
+    {
+        swap(str1, str2);
+    }
 
-	int n = str1.size();
-	int m = str2.size();
+    int n = str1.size();
+    int m = str2.size();
 
-	int t = 0;  // the flag of carry
-	string res = "";
-	for (int i = 0; i < m; ++i)
-	{
-		int currentValue = 0;  // sum of current digit
-		if (i < n)
-			currentValue = (str1[i] - '0') + (str2[i] - '0') + t;
-		else
-			currentValue = str2[i] - '0' + t;
-		res += to_string( currentValue % 10 );
-		t = currentValue / 10;
-	}
+    int t = 0;  // the flag of carry
+    string res = "";
+    for (int i = 0; i < m; ++i)
+    {
+        int currentValue = 0;  // sum of current digit
+        if (i < n)
+            currentValue = (str1[i] - '0') + (str2[i] - '0') + t;
+        else
+            currentValue = str2[i] - '0' + t;
+        res += to_string( currentValue % 10 );
+        t = currentValue / 10;
+    }
 
-	if (t > 0)
-	{
-		res += "1";
-	}
+    if (t > 0)
+    {
+        res += "1";
+    }
 
-	return res;
+    return res;
 }
